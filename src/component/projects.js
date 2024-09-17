5a13e4ed7a7c4cfe56cde4e9da4348d1c768c5be
AhmedSobh <https://github.com/AhmedSobh/Qraan.git>
2024-09-17 22:38:49 +0300

    LEdit2

--- src/component/projects.js
+++ src/component/projects.js
@@ -3,7 +3,7 @@ import "../style/style.css";
 import DentellaVideo from"../imgs/imgs/Dentella.mp4"
 import NtfsVideo from"../imgs/imgs/Ntfs.mp4"
 import RestaurantVideo from"../imgs/imgs/Rest.mp4"
-const ProjectCard = ({ title, description, video,gitHub }) => {
+const ProjectCard = ({ title, description, video,gitHub ,Demo}) => {
     const videoRef = useRef(null);
 
     const handleMouseEnter = async () => {
@@ -27,7 +27,11 @@ const ProjectCard = ({ title, description, video,gitHub }) => {
             <div className="left">
                 <h1>{title}</h1>
                 <h3>{description}</h3>
-                <a href={gitHub} target="blank"><button className="btn-demo">Visit GitHub</button></a>
+                <div className="btn-group">
+                    <a href={gitHub} target="blank"><button className="btn-demo">Visit GitHub</button></a>
+                    <a href={Demo} target="blank"><button className="btn-demo">Visit Demo</button></a>
+
+                </div>
             </div>
             <div className="right">
                 <div className="project-video-container"
@@ -52,18 +56,21 @@ const Projects = () => {
             <ProjectCard 
                 title="Dentella" 
                 gitHub="https://github.com/AhmedSobh/DentellaApp2.git" 
+                Demo="https://dentella-app2.vercel.app/" 
                 video={DentellaVideo}
                 description="Dentella is a platform designed to provide job opportunities for dental students, enabling them to connect with patients and offer low-cost or free treatments to help them pass their practical exams. Key features include allowing students to submit articles to aid patients, enabling patients to post requests for student assistance" 
             />
             <ProjectCard 
                 title="Restaurant" 
                 gitHub="https://github.com/AhmedSobh/Restaurant.git" 
+                Demo="https://restaurant-ochre-ten.vercel.app/" 
                 video={RestaurantVideo}
                 description=" created a restaurant website that showcases popular dishes and their prices. Users can log in to access additional features like adding dishes to their favorites and purchasing them. The main page highlights the most popular items, providing an engaging browsing experience." 
             />
             <ProjectCard 
                 title="NFTs" 
                 gitHub="https://github.com/AhmedSobh/NFTs-.git" 
+                Demo="https://ntfs-mauve.vercel.app/" 
                 video={NtfsVideo}
                 description="I created a project that displays popular NFT images and their prices. The main page includes sections for pages, portfolio, shop, and blog, along with user statistics and total sales figures. Users can view and place bids on NFTs from this collection. The goal is to provide an engaging and informative platform for NFT enthusiasts." 
             />
